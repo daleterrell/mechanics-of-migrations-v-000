acf7ea35a63cfe2357fb69c65d6410aaf57f5998
Dale Terrell <tragicid@gmail.com>
2018-07-16 19:52:49 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -74,3 +74,11 @@ end
  
 ActiveRecord::Base.connection.execute(sql)
 end
+
+require 'bundler/setup'
+Bundler.require
+ 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
