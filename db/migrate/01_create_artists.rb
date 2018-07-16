2383ee998c97be9b77a077417ae87801642eb7a6
Dale Terrell <tragicid@gmail.com>
2018-07-16 19:59:39 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,22 @@
-
+mechanics-of-migrations-v-000/
+  config/
+    environment.rb
+  db/
+    migrate/
+      01_create_artists.rb
+  spec/
+    artist_spec.rb
+    spec_helper.rb
+  .gitignore
+  .learn
+  .rspec
+  artist.rb
+  CONTRIBUTING.md
+  Gemfile
+  Gemfile.lock
+  LICENSE.md
+  Rakefile
+  README.md
 
 class CreateArtists < ActiveRecord::Migration
   def up
@@ -67,4 +85,3 @@ ActiveRecord::Base.establish_connection(
 
 class Artist 
 end
-
