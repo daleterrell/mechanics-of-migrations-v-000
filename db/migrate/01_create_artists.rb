e9cfaf2fc28d7edab4ebdb7019267b65089ed888
Dale Terrell <tragicid@gmail.com>
2018-07-16 20:00:23 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -85,3 +85,12 @@ ActiveRecord::Base.establish_connection(
 
 class Artist 
 end
+
+a = Artist.new(name: 'Jon')
+# => #<Artist id: nil, name: "Jon", genre: nil, age: nil, hometown: nil>
+ 
+a.age = 30
+# => 30
+ 
+a.save
+# => true
